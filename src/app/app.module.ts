e8fc63db99d5063ba756439a371c7d71ed399f9f
OlekVano <olekvano@gmail.com>
2023-05-21 10:29:17 +0100

    Added navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BotLinkComponent } from './bot-link/bot-link.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
+    NavigationComponent,
+    BotLinkComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bot-link/bot-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bot-link',
+  templateUrl: './bot-link.component.html',
+  styleUrls: ['./bot-link.component.less']
+})
+export class BotLinkComponent {
+
+}
